1898cf7ca7ce6f203342d71354f4d458c458f96a
PathPilot1112 <m22327803@gmail.com>
2026-04-10 15:42:09 +0530

    uc2 done

---
+++ src/main/TrainConsistManagementApp.java
@@ -0,0 +1,31 @@
+package main;
+import java.util.ArrayList;
+public class TrainConsistManagementApp {
+
+
+
+        public static void main(String[] args) {
+
+            ArrayList<String> passengerBogies = new ArrayList<>();
+
+            passengerBogies.add("Sleeper");
+            passengerBogies.add("AC Chair");
+            passengerBogies.add("First Class");
+
+            System.out.println("Passenger Bogies after addition:");
+            System.out.println(passengerBogies);
+
+            passengerBogies.remove("AC Chair");
+
+            System.out.println("\nPassenger Bogies after removal:");
+            System.out.println(passengerBogies);
+
+            boolean exists = passengerBogies.contains("Sleeper");
+
+            System.out.println("\nDoes Sleeper bogie exist? " + exists);
+
+            System.out.println("\nFinal Passenger Bogies List:");
+            System.out.println(passengerBogies);
+        }
+
+}
